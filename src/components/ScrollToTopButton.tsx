ce3a40910ad7b61804c1b872a3b8a36cfd0a1189
Chutirat Chaisan <donotdont1990@hotmail.com>
2024-06-21 12:43:42 +0700

    Add goto Top

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -3,13 +3,9 @@
 import { useEffect, useState } from "react";
 import Fab from "@mui/material/Fab";
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { motion, useInView, useScroll, Variant, useAnimationControls } from "framer-motion";
-
-interface ScrollToOptions {
-    left?: number;
-    top?: number;
-    behavior?: "auto" | "smooth";
-}
+import Fade from '@mui/material/Fade';
+import { useScroll, useAnimationControls } from "framer-motion";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function ScrollToTopButton() {
 
@@ -20,11 +16,6 @@ export default function ScrollToTopButton() {
         if (!isBrowser()) return;
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
-
-    const ScrollToTopContainerVariants: any = {
-        hide: { opacity: 0, y: 100 },
-        show: { opacity: 1, y: 0 },
-    };
 
     const { scrollYProgress } = useScroll();
     const controls = useAnimationControls();
@@ -41,28 +32,16 @@ export default function ScrollToTopButton() {
         });
     });
 
-    /*useEffect(() => {
-        const toggleVisibility = () => {
-          // if the user scrolls down, show the button
-          window.scrollY > 150 ? setIsVisible(true) : setIsVisible(false);
-        }
-        // listen for scroll events
-        window.addEventListener("scroll", toggleVisibility);
-    
-        // clear the listener on component unmount
-        return () => {
-          window.removeEventListener("scroll", toggleVisibility);
-        }
-      }, [])*/
-
-    return (
-        isVisible && <Fab
-            size="small"
-            color="default"
-            aria-label="Go to Top"
-            onClick={scrollToTop}
-            sx={{ position: "fixed", bottom: 16, right: 16, backgroundColor: "rgb(255 245 235)", border: "1px solid #ff5722" }}>
-            <KeyboardArrowUpIcon sx={{ color: "#ff5722" }} />
-        </Fab>
-    )
+    return (<Fade in={isVisible}>
+        <Tooltip placement="left" title="Scroll to top">
+            <Fab
+                size="small"
+                color="default"
+                aria-label="Go to Top"
+                onClick={scrollToTop}
+                sx={{ position: "fixed", bottom: 16, right: 16, backgroundColor: "rgb(255 245 235)", border: "1px solid #ff5722" }}>
+                <KeyboardArrowUpIcon sx={{ color: "#ff5722" }} />
+            </Fab>
+        </Tooltip>
+    </Fade>)
 }
